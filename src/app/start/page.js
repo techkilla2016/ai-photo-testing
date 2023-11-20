b6cf93019ebf3d69092bc9824364d05532ca1960
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-11-20 15:21:47 +0530

    change API URL

--- src/app/start/page.js
+++ src/app/start/page.js
@@ -78,7 +78,7 @@ const Home = () => {
         try {
             // console.log(imgFile)
             // axios.post('https://1a53-103-17-110-126.ngrok-free.app/rec', {
-            axios.post('https://2964-103-17-110-97.ngrok-free.app/rec', {
+            axios.post('https://b723-103-17-110-127.ngrok-free.app/rec', {
                 image: imgFile.split(',')[1],
                 choice: select
             }).then(res => {
