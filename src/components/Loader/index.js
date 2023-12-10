d12831082b1e1d4f464e7d544963c6a28dfc74cd
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-12-10 22:02:55 +0530

    compress file

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
     return (
         <div className='loaders'>
-            <div class="spinner-border" style={{ width: '3rem', height: '3rem' }} role="status">
+            <div className="spinner-border" style={{ width: '3rem', height: '3rem' }} role="status">
                 
             </div>
         </div>
